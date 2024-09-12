478c018a835fc4865b204ca5044f626545577cab
吉岡優之介 <yu@yoshiokayuunosukenoMac-Studio.local>
2024-09-12 22:30:38 +0900

    map処理

--- components/Links.js
+++ components/Links.js
@@ -1,35 +1,40 @@
 import styles from './Links.module.css';
 
+const ITEMS =[
+    {
+      href: "https://nextjs.org/docs",
+      title: "Documentation",
+      description: "Find in-depth information about Next.js features and API."
+    },
+    {
+      href: "https://nextjs.org/learn",
+      title: "Learn",
+      description: "Learn about Next.js in an interactive course with quizzes!"
+    },
+    {
+      href: "https://github.com/vercel/next.js/tree/canary/examples",
+      title: "Examples",
+      description: "Discover and deploy boilerplate example Next.js projects."
+    },
+    {
+      href: "https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app",
+      title: "Deploy",
+      description: "Instantly deploy your Next.js site to a public URL with Vercel."
+    }
+]
+
 export function Links() {
   return (
     <div className={styles.grid}>
-      <a href="https://nextjs.org/docs" className={styles.card}>
-          <h3>Documentation &rarr;</h3>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
 
-        <a href="https://nextjs.org/learn" className={styles.card}>
-          <h3>Learn &rarr;</h3>
-          <p>Learn about Next.js in an interactive course with quizzes!</p>
-        </a>
-
-        <a
-          href="https://github.com/vercel/next.js/tree/canary/examples"
-          className={styles.card}
-        >
-          <h3>Examples &rarr;</h3>
-          <p>Discover and deploy boilerplate example Next.js projects.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          className={styles.card}
-        >
-          <h3>Deploy &rarr;</h3>
-          <p>
-            Instantly deploy your Next.js site to a public URL with Vercel.
-          </p>
-        </a>
-      </div>
+      {ITEMS.map((item) => {
+        return (
+          <a href={item.href} className={styles.card} key={item.href}>
+            <h3>{item.title} &rarr;</h3>
+            <p>{item.description}</p>
+          </a>
+        )})
+      }
+    </div>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -19,7 +19,6 @@ export default function Home() {
 
       <Footer />
 
-
       <style jsx global>{`
         html,
         body {
